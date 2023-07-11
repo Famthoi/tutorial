cb36f1deb4c1e82bb7eea69881f28ea7f3476308
Famthoi <mf5172@srmist.edu.in>
2023-07-11 12:51:04 -0400

    tutorial

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    float lastSpawn;
+    float spawnCooldown = 1.0f;
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastSpawn += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& lastSpawn>=spawnCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawn = 0;
+        }
+    }
+}
